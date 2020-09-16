6f683269214c3edeb8f43ddc8bb7fc7877f598c9
RAJWINDER19 <71091611+RAJWINDER19@users.noreply.github.com>
2020-09-15 20:11:34 -0700

    Update Q1.cpp

--- A1/Q1.cpp
+++ A1/Q1.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int speed {20};
+    int time  {10};
+    int distance;
+    
+    distance = speed*time;
+    cout<< " the distance is "<< distance <<endl;
+    return 0;
+    
+}
