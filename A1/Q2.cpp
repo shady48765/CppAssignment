7e640499c6b5186aa5141b92a8425fa717b79177
RAJWINDER19 <71091611+RAJWINDER19@users.noreply.github.com>
2020-09-15 20:12:52 -0700

    Update Q2.cpp

--- A1/Q2.cpp
+++ A1/Q2.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    float force{172.5};
+    float area  {27.5};
+    float pressure;
+    
+    pressure= area/force;
+    cout<< " the pressure is "<< pressure <<endl;
+    return 0;
+    
+}
