7f59aef3fdc716574fbdc73b5e139b621d0c65a9
RAJWINDER19 <71091611+RAJWINDER19@users.noreply.github.com>
2020-09-26 20:44:32 -0700

    Update Q31.cpp

--- A2/Q31.cpp
+++ A2/Q31.cpp
@@ -1,1 +1,32 @@
 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int selection;
+  cout << "Which formula do you want to see?\n";
+  cout << "1. Area of a circle\n";
+  cout << "2. Area of a rectangle\n";
+  cout << "3. Area of a cylinder\n";
+  cout << "4. None of them!\n";
+  cin >> selection;
+  
+  switch(selection)
+  {
+      case 1:
+       cout<< " Pi times radius squared\n ";
+       break;
+      case 2:
+       cout<< " Length times width\n";
+       break;
+      case 3:
+       cout<< " Pi times radius sqaured times height\n";
+       break;
+      case 4:
+       cout<< " Okay good bye!\n";
+       break;
+      default:
+       cout<< "Not good with numbers, eh?\n";
+  }
+}
